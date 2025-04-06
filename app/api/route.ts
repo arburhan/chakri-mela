23ea564107b35c3c26654d89d8244bb39f9f27d0
arburhan <burhan128454@outlook.com>
2025-04-06 18:05:12 +0600

    add endpoint for checking

---
+++ app/api/route.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import connectDB from "@/utils/connectDB";
+
+export async function GET() {
+    try {
+        await connectDB();
+        console.log("Database connected successfully.......");
+        return new Response(JSON.stringify({ message: "Alhamdulillah......" }), { status: 200 });
+
+    } catch (error) {
+        console.error("Error fetching.........:", error);
+        return new Response(JSON.stringify({ message: "Internal server error" }), { status: 500 });
+    }
+}
